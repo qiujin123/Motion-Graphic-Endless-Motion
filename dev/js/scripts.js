1a1019e849f1b4f3ba9d16a590d8f08e49476806
qiujin123 <qiujinzou123@gmail.com>
2021-04-09 18:03:21 -0400

    scripts p2

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,14 +1,16 @@
 import { gsap } from "gsap";
+// import $ from jquery;
+
+// let shapeTl = gsap.timeline({paused:true})
+let shapeTl = gsap.timeline();
+
+shapeTl.from("#circle", {duration: 1})
+        .to("#circle", {duration: 1, rotate: 360, x:100})
 
 
-let shapeTl = gsap.timeline({paused:true})
-
-shapeTl.from("#circle", {duration: 1, x:10})
 
 
 
-
-
-$(".click").on("click",function(){
-    shapeTl.play();
-})
+// $(".click").on("click",function(){
+//     shapeTl.play();
+// })
